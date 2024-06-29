a1c4a0e5f11b6cc286d3ceb8eecd93276bdf0311
harud3 <haruto_hana_0409@icloud.com>
2024-06-29 14:19:36 +0900

    デッキ編成画面　ヒーローで使えるカードを縛る

--- Phantom-Phoenix/Assets/Scripts/EditDeckScripts/EditDeckScripts.cs
+++ Phantom-Phoenix/Assets/Scripts/EditDeckScripts/EditDeckScripts.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -14,10 +15,15 @@ public class EditDeckManager : MonoBehaviour
     [SerializeField] GameObject cardPrefab;
     [SerializeField] Transform Stock;
     [SerializeField] Transform Deck;
+    List<CardEntity> stockCards;
+    List<int> cardIDs;
     private int pages = 1;
     private void Start()
     {
-        
+        var heroID = GameDataManager.instance.editDeckHeroID;
+        stockCards = GameDataManager.instance.cardlist.cl.Where(c => c.hero == CardEntity.Hero.common || (int)c.hero == heroID).OrderByDescending(c => c.hero).ThenBy(c => c.ID).ToList();
+        cardIDs = stockCards.Select(c => c.ID).ToList();
+
         ButtonLeft.onClick.AddListener(() =>
         {
             AudioManager.instance.SoundCardMove();
@@ -29,24 +35,29 @@ public class EditDeckManager : MonoBehaviour
             ButtonPageOnClick(false);
         });
 
+        DeckModel deckmodel = new DeckModel().Init();
+        if(deckmodel.useHeroID == GameDataManager.instance.editDeckHeroID)
+        {
+            deckmodel.deck.OrderBy(i => i).ToList().ForEach(i =>
+            {
+                Instantiate(cardPrefab, Deck).GetComponent<EditDeckCardController>().Init(i);
+            });
+        }
+
         //最初のページを表示
         GetNewStock();
 
-        DeckModel deckmodel = new DeckModel().Init();
-        deckmodel.deck.OrderBy(i => i).ToList().ForEach(i =>
-        {
-            Instantiate(cardPrefab, Deck).GetComponent<EditDeckCardController>().Init(i);
-        });
+        
     }
     private void GetNewStock()
     {
         int viewCount = 8;
-        int id = 1;
+        int id = 0;
 
-        if (GameDataManager.instance.cardlist.cl.Count is int clc && clc < pages * 8)
+        if (cardIDs.Count is int cc && cc < pages * 8)
         {
-            if (clc < (pages - 1) * 8) { pages--; } //表示するカードがないので、page--で対処
-            viewCount = clc % 8;
+            if (cc < (pages - 1) * 8) { pages--; } //表示するカードがないので、page--で対処
+            viewCount = cc % 8;
         }
 
         //今、カード一覧に表示されているカードを消す
@@ -58,7 +69,7 @@ public class EditDeckManager : MonoBehaviour
         do
         {
             EditDeckCardController card = Instantiate(cardPrefab, Stock).GetComponent<EditDeckCardController>();
-            card.Init((id++) + (8 * pages) - 8);
+            card.Init(cardIDs[(id++) + (8 * pages) - 8]);
         } while (--viewCount > 0);
     }
     private void ButtonPageOnClick(bool isLeft)
